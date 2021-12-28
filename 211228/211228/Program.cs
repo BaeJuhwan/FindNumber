ad8fca704a6ace3e0c33f8da1fc572bfebd0e7df
BaeJuhwan <bjw505@naver.com>
2021-12-28 23:11:30 +0900

    first commit

---
+++ 211228/211228/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _211228
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("첫번째 수 입력:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("두번째 수 입력:");
+            b = int.Parse(Console.ReadLine());
+            
+            if(a > b)
+            {
+                Console.WriteLine(a);
+            }
+            
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
